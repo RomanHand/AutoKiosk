c67b23d646a0272bca03f9cbd1dc2e740094848a
khandoshko.ra <khandoshko.ra@dns-shop.ru>
2025-04-21 15:52:34 +1000

    fix kiosk.py

--- kiosk.py
+++ kiosk.py
@@ -1,32 +1,39 @@
 #!/usr/bin/python3
-"""Script for run webview."""
-import webview
+"""Script for running a webview window with a URL."""
+
 import argparse
-import screeninfo
+import sys
+import webview
 
 def main():
+    # Default screen resolution
     resolution = [1920, 1080]
+    
+    # Argument parsing
     parser = argparse.ArgumentParser()
-    parser.add_argument('--url', metavar='str', type=str, help='site url')
-    parser.add_argument('--width', metavar='int', type=int, help='width screen',
-                        default=resolution[0])
-    parser.add_argument('--height', metavar='int', type=int, help='height screen',
-                        default=resolution[1])
-    parser.add_argument('--screen', metavar='int', type=int, help='number screen', default=0)
+    parser.add_argument('--url', metavar='str', type=str, help='Site URL')
+    parser.add_argument('--width', metavar='int', type=int, help='Screen width', default=resolution[0])
+    parser.add_argument('--height', metavar='int', type=int, help='Screen height', default=resolution[1])
+    parser.add_argument('--screen', metavar='int', type=int, help='Number of the screen', default=0)
 
     args = parser.parse_args()
-    if args.url == None:
-        exit("use --url <site>")
+
+    # Check if URL is provided
+    if args.url is None:
+        sys.exit("Error: You must provide a URL using --url <site>")
+
     url = args.url
     s_width = args.width
     s_height = args.height
 
-
+    # Get the selected screen
     n_screen = webview.screens[args.screen]
-    print(n_screen)
+    print(f"Using screen: {n_screen}")
+
+    # Create and start the webview window
     webview.create_window('Hello world', url, fullscreen=True, width=s_width, height=s_height,
-                        background_color="#000000", 
-                        on_top=True, screen=n_screen)
+                        background_color="#000000", on_top=True, screen=n_screen)
     webview.start()
 
-main()
+if __name__ == "__main__":
+    main()
